c70a45275dcad5f0e093d0d20fbc0571f50421f4
Martin D'Souza <martin.dsouza@gmail.com>
2016-07-08 23:17:15 -0600

    Major updates

--- poc.js
+++ poc.js
@@ -2,25 +2,66 @@ const Handlebars = require('./lib/handlebars.js');
 const path = require('path');
 const fs = require('fs');
 const extend = require('node.extend');
+const {execSync} = require('child_process');
 
-let config = JSON.parse(fs.readFileSync(path.resolve(__dirname + '/config.json'),'utf8'));
+
+const consoleRed = '\033[31m';
+
+
+var config = JSON.parse(fs.readFileSync(path.resolve(__dirname + '/config.json'),'utf8'));
 
 // TODO mdsouza: need to use args but for now testing
 // TODO mdsouza: Create a sample_config.json
-let project = config.projects.giffy;
+var project = config.projects.giffy;
 project = extend(true, {}, config.defaults, project);
 
 project.templateContent = {};
 
+// TODO mdsouza: allow for a custom SQL file to generate the SQL as MTAG has specific needs
 
 // TODO eventually we can get this data from SQLcl
 // See poc.sql for query to use
-let tableInfo = {"results":[{"columns":[{"name":"TABLE_NAME","type":"NUMBER"},{"name":"COLUMNS","type":"NUMBER"}],"items":
-[
-{"table_name":"DEMO_ORDERS","columns":[
-{"column_name":"ORDER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"CUSTOMER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"ORDER_TOTAL","data_type":"NUMBER","nullable":"Y"},{"column_name":"ORDER_TIMESTAMP","data_type":"TIMESTAMP(6) WITH LOCAL TIME ZONE","nullable":"Y"},{"column_name":"USER_NAME","data_type":"VARCHAR2","nullable":"Y"},{"column_name":"TAGS","data_type":"VARCHAR2","nullable":"Y"}]},{"table_name":"DEMO_ORDER_ITEMS","columns":[
-{"column_name":"ORDER_ITEM_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"ORDER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"PRODUCT_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"UNIT_PRICE","data_type":"NUMBER","nullable":"N"},{"column_name":"QUANTITY","data_type":"NUMBER","nullable":"N"}]}]}]}
-;
+// var tableInfo = {"results":[{"columns":[{"name":"TABLE_NAME","type":"NUMBER"},{"name":"COLUMNS","type":"NUMBER"}],"items":
+// [
+// {"table_name":"DEMO_ORDERS","columns":[
+// {"column_name":"ORDER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"CUSTOMER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"ORDER_TOTAL","data_type":"NUMBER","nullable":"Y"},{"column_name":"ORDER_TIMESTAMP","data_type":"TIMESTAMP(6) WITH LOCAL TIME ZONE","nullable":"Y"},{"column_name":"USER_NAME","data_type":"VARCHAR2","nullable":"Y"},{"column_name":"TAGS","data_type":"VARCHAR2","nullable":"Y"}]},{"table_name":"DEMO_ORDER_ITEMS","columns":[
+// {"column_name":"ORDER_ITEM_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"ORDER_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"PRODUCT_ID","data_type":"NUMBER","nullable":"N"},{"column_name":"UNIT_PRICE","data_type":"NUMBER","nullable":"N"},{"column_name":"QUANTITY","data_type":"NUMBER","nullable":"N"}]}]}]}
+// ;
+
+
+var stdout = execSync(
+  config.sqlcl + ' ' + project.connectionDetails + ' @' + project.sqlFile,
+  {
+    "pwd": path.resolve(__dirname),
+    "encoding": 'utf8'
+  });
+
+// This will be the string between START and END
+try{
+  var tableInfo = /(%%%START%%%)((.|\n)*)(%%%END%%%)/.exec(stdout)[2].trim();
+}
+catch (e) {
+  console.log(`${consoleRed} *** Error parsing SQL ***\n`);
+  console.log('Ensure that SQL output is wrapped with %%%START%%% and %%%END%%% ');
+  console.log(`\n\n Output: \n\n ${stdout}`);
+  process.exit(1);
+}
+
+
+// Convert it to JSON structure
+try{
+  tableInfo = JSON.parse(tableInfo);
+}
+catch(e){
+  console.log(`${consoleRed} *** Error converting SQL output to JSON ***\n`);
+  console.log('Ensure that "set sqlformat json" is set in your SQL file');
+  console.log(`\n\n Output: \n\n ${tableInfo}`);
+  process.exit(1);
+}
+
+// Filter to items level
+// This will return an array of data
+// Each array entry will represent a table.
 tableInfo = tableInfo.results[0].items;
 
 
